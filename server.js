0d5dc7cea2e824b449dc1b2b575ac93207cf9221
Ben Postlethwaite <post.ben.here@gmail.com>
2012-06-12 17:06:44 -0700

    merging

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var server = require("node-static")
 // Set logging level
 io.set('log level', 1)
 // Listen on port
-var port = 80
+var port = 8081
 app.listen(port)
 console.log("Static server listening on " + port )
 //
