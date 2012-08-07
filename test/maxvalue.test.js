4836b651a6c8b9318ad90ead993e8c5053d2a718
Ben Postlethwaite <post.ben.here@gmail.com>
2012-08-07 11:24:04 -0700

    ready to add mode switchers

--- test/maxvalue.test.js
+++ test/maxvalue.test.js
@@ -1,8 +1,12 @@
 /*jshint asi: true*/
 /*jshint laxcomma: true*/
 "use strict";
-var field = require("../public/physics.engine.js")()
-var printarr = require("../printArray.js")
+var field = require("pde-engine")({
+    dt: 0.1
+  , gamma: 0.02
+  , eqn: "wave"
+  })
+var printarr = require("./printArray.js")
 
 var m = 25
 var n = 25
@@ -16,18 +20,20 @@ for (i = 0; i <= 200; ++i) {
 
 field.setResolution(m, n)
 
+field.addSource(5 , 5, 35)
 
 for (i = 0; i < 500; i++) {
-  var u = field.diffusionUpdate()
+  var u = field.update()
   maxval(u)
   distribution(u)
   var row = Math.floor( Math.random() * m  )
   var col = Math.floor( Math.random() * n  )
-  field.addDroplet(row , col, 35)
+
+  console.log(u[15][15])
 }
 
-console.log(dist)
-console.log(absmax)
+//console.log(dist)
+//console.log(absmax)
 function maxval (arr) {
 	var i, j, max = 0
 	for (i = 0; i < m; ++i) {
